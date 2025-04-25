b6ebc6c675c1d0667d20fb8c3e74d868a2e1ee3e
Chinmay Shet <chishxd@proton.me>
2025-04-25 09:11:59 +0000

    Added animations

---
+++ src/Components/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2>Education</h2>
+      <div className="education">
+        <div className="education-item">
+          <h3>Diploma in AIML</h3>
+          <p>New Institute of Technology, Kolhapur</p>
+          <p>Graduating in: 2026</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Education;
